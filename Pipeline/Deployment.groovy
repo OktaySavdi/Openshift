0ada48465758242b9634df4cc88cb87ff7dcbf26
Oktay.Savdi <oktay.savdi@gmail.com>
2020-10-13 10:16:56 +0300

    Create Deployment.groovy

---
+++ Pipeline/Deployment.groovy
@@ -0,0 +1,65 @@
+pipeline {
+    //Servers
+    agent any
+
+    //Parameters
+    parameters {
+        string(name: 'PROJECT_NAME', defaultValue: '', description: 'Proje Adi')
+        string(name: 'APP_NAME', defaultValue: '', description: 'Uygulama Adi (ör: cbot ) ')
+        string(name: 'IMAGE_NAME', defaultValue: '', description: 'Imaj Adi (ör: nginx:1.19.0 ) ')
+    }
+
+    stages{        
+        stage('control')
+        {
+            steps
+            {
+                script
+                {
+                    if (!params.PROJECT_NAME.isEmpty())
+                    { 
+                        if (!params.APP_NAME.isEmpty())
+                        { 
+                            if (params.IMAGE_NAME.isEmpty())
+                            {  error('Imaj Alani Boş') }
+                        }else{error('Uygulama Alani Boş')}
+                    }else{error('Proje Alani Boş')}                              
+                }
+            }    
+        }
+            stage('action'){
+                steps{
+                        script
+                             {
+								withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'myuser_creds', usernameVariable: 'MY_USER', passwordVariable: 'MY_PASS'],]) {
+									sh '''
+										project_name=`echo "${PROJECT_NAME}" | tr '[:upper:]' '[:lower:]'`
+										app_name=`echo "${APP_NAME}" | tr '[:upper:]' '[:lower:]'`
+                                                                                image_name=$(echo $IMAGE_NAME | cut -f1 -d:)
+										APP=$(if [[ $(oc get deployment ${APP_NAME} -n $project_name | wc -l) -gt 0 ]]; then echo "deployment"; elif [[ $(oc get dc ${APP_NAME} -n $project_name  | wc -l) -gt 0 ]]; then echo "dc"; else echo "Uygulama Bulunamadı"; exit 1 ; fi)
+										oc login https://api.hb.oc.local:6443 --username=${MY_USER} --password=${MY_PASS} --insecure-skip-tls-verify=true
+										
+										if [ $(oc get project "${PROJECT_NAME}" | wc -l) -gt 0 ]; then
+											
+											oc project $project_name
+											
+											if [[ $(oc get ${APP} ${APP_NAME} | wc -l) -gt 0 ]]; then
+
+												oc patch ${APP}/${APP_NAME} --patch="{\\\"spec\\\":{\\\"template\\\":{\\\"spec\\\":{\\\"containers\\\":[{\\\"name\\\": \\\"${image_name}\\\", \\\"image\\\":\\\"${IMAGE_NAME}\\\"}]}}}}"
+												oc rollout status ${APP}/${APP_NAME} -n ${PROJECT_NAME} --watch		
+
+											else
+												echo "Deployment $APP_NAME Bulunamadi"
+												exit 3
+											fi
+										else
+											echo "Proje $PROJECT_NAME Bulunamadi"
+											exit 2
+										fi
+									'''
+								}
+                            }
+                    }
+            }
+    }
+}
